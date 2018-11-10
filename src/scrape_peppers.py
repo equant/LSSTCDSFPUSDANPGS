6282bd567839498eadfaa28fd19cb0be28a74edd
Nathanial Hendler <nathanhendler@gmail.com>
2018-11-10 14:58:04 -0700

    AAAAAAAAAHgit add .git add .!

--- src/scrape_peppers.py
+++ src/scrape_peppers.py
@@ -6,6 +6,8 @@ from requests import get
 from requests.exceptions import RequestException
 from contextlib import closing
 from bs4 import BeautifulSoup
+import numpy as np
+import pandas as pd
 
 raw_html = open('capsicum_accessions.html').read()
 html = BeautifulSoup(raw_html, 'html.parser')
@@ -16,11 +18,13 @@ foo = [x for x in foo if "accessiondetail" in x.attrs['href']]
 
 data = [(x.attrs['href'], x.string) for x in foo]
 
-import pandas as pd
-
-TESTING = False
+TESTING = True
 if TESTING:
-    data = data[:5]
+    #my_test_url = 'https://npgsweb.ars-grin.gov/gringlobal/accessiondetail.aspx?id=1047133'
+    #data = np.array(data)
+    #_idx = np.where(data[:,0] == my_test_url)
+    #data = data[_idx]
+    data = data[:600]
 
 df = pd.DataFrame(data, columns=['url', 'plant_id'])
 df['id'] = [int(x.split('?')[-1].split("=")[-1]) for x in df['url']] 
@@ -49,10 +53,26 @@ for plant_id, row in df.iterrows():
         html = BeautifulSoup(response.content, 'html.parser')
         tables = html.select('table')
         evaluation_table = tables[-2]
-        column_names = evaluation_table.select("tr")[1].select("th")[1:]
-        column_names = [x.string for x in column_names]
-        values       = evaluation_table.select("tr")[2].select("td")
-        values       = [x.string for x in values]
+        #column_names = evaluation_table.select("tr")[1].select("th")[1:]
+        #column_names = [x.string for x in column_names]
+        all_values       = evaluation_table.select("tr")[2].select("td")
+        all_values       = [x.string for x in all_values]
+
+        column_table_headers = evaluation_table.select("tr")[1].select("th")[1:]
+        saved_values = []
+        column_names = []
+        col_count    = 0
+        for _c_idx, th in enumerate(column_table_headers):
+            n_colspan = int(th.attrs['colspan'])
+            column_name = th.string
+            column_names.append(column_name)
+            col_count += n_colspan - 1
+            value = all_values[col_count]
+            saved_values.append(value)
+            print(f"{_c_idx:6} - {col_count} - {column_name:20} {value:20}")
+            col_count += 1
+        values = saved_values
+
     except IndexError:
         print(f"No features for this accession")
         df_full.loc[plant_id, 'error'] = 'no features'
@@ -67,15 +87,19 @@ for plant_id, row in df.iterrows():
         value_full = values[column_idx]
         df_full.loc[plant_id, column] = value_full
         if (value_full != '0 - ABSENT'):
-            value_short = value_full[0]
-            df.loc[plant_id, column] = value_short
+            try:
+                value_short = value_full.split("-")[0]
+                value_short = float(value_short)
+                df.loc[plant_id, column] = value_short
+            except:
+                print(f"Was not able to save {value_full} as a short value")
 
     count += 1
     print(f"Sleep... {count}/{len(df)}")
-    sleep(randint(2,7))
+    sleep(randint(0,2))
 
 df.to_csv(f"capsicum_ml_{len(df)}.csv")
-df_full.csv(f"capsicum_full_{len(df_full)}.csv")
+df_full.to_csv(f"capsicum_full_{len(df_full)}.csv")
 
 #for url, acc_id in data[:1]:
 
